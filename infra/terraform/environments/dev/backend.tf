331d4882880d953db9233362bd3c847afcd0abd2
Mahabub Ahmed <mahabub.ahmed.eee@gmail.com>
2026-06-14 21:55:37 +0200

    infa: backend.tf update for deprecated issue

--- infra/terraform/environments/dev/backend.tf
+++ infra/terraform/environments/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-    backend "s3" {
-        bucket = "todo-list-terraform-state"
-        key    = "dev/terraform.tfstate"
-        region = "eu-north-1"
-        encrypt = true
-        dynamodb_table = "todo-list-terraform-locks"
-    }
+  backend "s3" {
+    bucket       = "todo-list-terraform-state"
+    key          = "dev/terraform.tfstate"
+    region       = "eu-north-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
